ac8ec6be6e36ca4b2ec06201d7f20e4f1e6a1c0a
GABSSSTX <gabuche2006@gmail.com>
2026-02-25 15:52:53 +0000

    se puso el menu base

---
+++ usuario.java
@@ -0,0 +1,28 @@
+
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String email;
+
+    public Usuario(int id, String nombre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Este es mi nombre "+ nombre y este mi correo + email;
+
+        public.int.getId()  {return id;}
+        public.int.getNombre()  {return nombre;}
+        public.int.getEmail()  {return email;}
+
+        public.int.setNombre(int id )
+        public.int.setNombre(int id )
+        public.int.setNombre(int id )
+
+
+    }
+}
+      
